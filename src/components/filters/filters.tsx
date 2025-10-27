2b876bd2771880f793743df1699a85198334a969
Your Name <your.email@example.com>
2025-10-27 19:26:24 +0300

    Добавление адаптивности

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -6,11 +6,11 @@ type FiltersProps = {
 };
 const Filters = ({activeFilter, setActiveFilter} : FiltersProps) => {
     return (
-        <div className="flex items-center mt-3 mb-6 gap-4">
+        <div className="flex items-center justify-center flex-wrap mt-3 mb-6 gap-4">
            {FILTERS.map((filter) => (
                <button
                    key={filter}
-                   className={`px-4 py-2 rounded-md text-sm font-medium transition select-none focus:outline-none focus:ring-offset-2  duration-300 ease-in-out ${activeFilter === filter ? 'bg-blue-400 text-white' : 'bg-gray-800 text-white hover:bg-blue-300'}`}
+                   className={`md:px-4 md:py-2 px-2 py-1 rounded-md text-sm font-medium transition select-none focus:outline-none focus:ring-offset-2  duration-300 ease-in-out ${activeFilter === filter ? 'bg-blue-400 text-white' : 'bg-gray-800 text-white hover:bg-blue-300'}`}
                    onClick={() => setActiveFilter(filter)}
                >
                    {filter}
