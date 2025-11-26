d5a828635824870968b15a9a4f63f42550cc2e14
Vladimir <vlad.stavros@bk.ru>
2025-11-26 19:35:09 +0300

    Saga для логина

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,20 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {favoriteReducer} from './favorite/favoriteSlice';
 import { authReducer } from './auth/userSlice';
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './root-saga.ts'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const rootReducer = combineReducers({
+    favorite: favoriteReducer,
+    auth: authReducer
+})
 
 export const store = configureStore({
-    reducer: {
-        favorite: favoriteReducer,
-        auth: authReducer
-    }
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({thunk: false}).concat(sagaMiddleware ),
 });
 
+sagaMiddleware.run(rootSaga)
 export type RootState = ReturnType<typeof store.getState>;
