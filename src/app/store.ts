798e68c2c60dc4d8b3dd10c8fe48997480083489
Vladimir <vlad.stavros@bk.ru>
2025-11-29 21:06:58 +0300

    Базовая установка

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {favoriteReducer} from './favorite/favoriteSlice';
 import { authReducer } from './auth/userSlice';
+import { githubApi } from '../services/api';
+
 
 export const store = configureStore({
     reducer: {
+        [githubApi.reducerPath]: githubApi.reducer,
         favorite: favoriteReducer,
         auth: authReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(githubApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
